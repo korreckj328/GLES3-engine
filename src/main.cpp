6915531ad1f0b457a658f27fd1a8092ace522532
Jeremiah Korreck <korreckj328@gmail.com>
2023-04-25 11:10:55 -0400

    moving to gles3 sdl2

--- src/main.cpp
+++ src/main.cpp
@@ -6,59 +6,85 @@ single board arm devices that can usually run desktop gl 2.1.
 */
 
 #include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_opengles2.h>
+#include <GLES3/gl3.h>
+#include <cstdio>
+#include <cstdlib>
 
+const unsigned int DISP_WIDTH = 640; 
+const unsigned int DISP_HEIGHT = 480;
 
-
-void renderScene();
-void initGame();
-void updateKeyboard(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main() {
-    glfwInit();
-
-    // Set all the required options for GLFW
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 2 );
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
-    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
-
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Simple GLES Engine", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, updateKeyboard);
-    glewInit();
-    initGame();
-
-    while (!glfwWindowShouldClose(window)) {
-
-        renderScene();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
+    // The window
+    SDL_Window *window = NULL; 
+    // The OpenGL context
+    SDL_GLContext context = NULL;
+    // Init SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
     }
-    glfwTerminate();
+
+    // setup the exit hook
+    atexit(SDL_Quit);
+
+    // init sdl
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    // Request OpenGL ES 3.0
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES); 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3); 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    
+    // Want double-buffering
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    // Create the window
+    window = SDL_CreateWindow("GLES3+SDL2 Tutorial",
+                                SDL_WINDOWPOS_UNDEFINED, 
+                                SDL_WINDOWPOS_UNDEFINED, 
+                                DISP_WIDTH, 
+                                DISP_HEIGHT, 
+                                SDL_WINDOW_OPENGL);
+    if (!window) { 
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, 
+                                "Error",
+                                "Couldn't create the main window.", 
+                                NULL); 
+        return EXIT_FAILURE;
+    }
+
+    context = SDL_GL_CreateContext(window);
+
+    if (!context) { 
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, 
+                                "Error",
+                                "Couldn't create an OpenGL context.", 
+                                NULL); 
+        return EXIT_FAILURE;
+    }
+
+    // Clear to black
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); glClear(GL_COLOR_BUFFER_BIT);
+    // Update the window
+    SDL_GL_SwapWindow(window);
+
+    // Wait for the user to quit
+    bool quit = false; 
+    while (!quit) {
+        SDL_Event event;
+        if (SDL_WaitEvent(&event) != 0) { 
+            if (event.type == SDL_EVENT_QUIT) {
+                // User wants to quit
+                quit = true; 
+            }
+        } 
+    }
+
     return 0;
-}
-
-void renderScene(){
-    glClearColor(0.0, 0.0, 0.0, 0.0); 
-    glClearDepth(1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
-
-void initGame() {
-    glEnable(GL_DEPTH_TEST);
-}
-
-
-void updateKeyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-    if (true) {
-        if ((key == GLFW_KEY_UP || key == GLFW_KEY_SPACE) && action == GLFW_PRESS) {
-            printf("pressed up key \n");
-        }
-    }
 }
